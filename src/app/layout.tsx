611b374dc6e28056d9a228a617e88d4790a09930
iubns <iubns@naver.com>
2025-01-14 14:35:24 +0900

    test: build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
