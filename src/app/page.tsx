fc033577ccfead49371d721f2dab276fe1fc5d3d
HyungEun <116543097+hekim-cse@users.noreply.github.com>
2025-01-19 00:08:11 +0900

    오류 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import TestBox from "./components/TestBox"
 
 export default function Home() {
-  let a = 1
-  let b = 2
+  const a = 1
+  const b = 2
 
   return (
     <div className="text-xl flex justify-between p-10">
